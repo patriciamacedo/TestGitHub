64ecb0293bc11ebd80952a72cb03411f3324a340
Wesley <202201003@estudantes.ips.pt>
2022-11-21 22:45:05 +0000

    CREATE CLASSE C1

---
+++ src/com/pm/C1.java
@@ -0,0 +1,9 @@
+package com.pm;
+
+public class C1 {
+    public void m1 (int x){
+        for (int i = 0; i < x; i++)
+            System.out.println ("WESLEY");
+    }
+
+}
--- src/com/pm/Main.java
+++ src/com/pm/Main.java
@@ -3,6 +3,11 @@ package com.pm;
 public class Main {
 
     public static void main(String[] args) {
+
+
+
+
+
         System.out.println("HELLO");
     }
 }
