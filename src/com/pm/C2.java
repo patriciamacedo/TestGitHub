1640ac22d0ca862ff7b7828078dcab0a902120fe
KialaIPS <kiala.mario@hotmail.com>
2022-11-21 23:09:14 +0000

    Alterei o nome

--- src/com/pm/C2.java
+++ src/com/pm/C2.java
@@ -3,7 +3,7 @@ package com.pm;
 public class C2 {
     public void m1(int x){
         for(int i=0;1<x;i++)
-            System.out.println("Kialammm");
+            System.out.println("Kialm");
     }
 }
 
