aa129a8f900866a7f6285cf7225da6afd269ae8e
João <201701827@estudantes.ips.pt>
2022-11-21 23:12:17 +0000

    Commit C3

--- src/com/pm/C3.java
+++ src/com/pm/C3.java
@@ -4,7 +4,7 @@ public class C3 {
 
     public void m1(int x) {
         for (int i=0 ; 1< x; i++) {
-            System.out.println("JoaoRibs");
+            System.out.println("Joao");
         }
     }
 }
